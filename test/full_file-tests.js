6f15ab3f472296c5be39ddae7c320e65904394d7
Danny Su <contact@dannysu.com>
2017-08-20 12:48:42 -0700

    Remove describe.only

--- test/full_file-tests.js
+++ test/full_file-tests.js
@@ -10,7 +10,7 @@ const orig = `${__dirname}/fixtures/orig`;
 const annotated = `${__dirname}/fixtures/annotated`;
 
 describe('full file', function() {
-    describe.only('test1', function() {
+    describe('test1', function() {
         it('should convert correctly', function() {
             const code = fs.readFileSync(`${orig}/test1.js`).toString();
             const modifiedCode = converter.convertSourceCode(code);
