ea853af1a7e67d0acd87ac77dbffbba903efdae8
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-01-22 04:19:11 -0600

    Add test for discarding line comments

---
+++ test/fixtures/orig/test3.js
@@ -0,0 +1,18 @@
+/**
+ * Returns the opacity value to use when rendering the `Minimap` text.
+ *
+ * @return {Number} the text opacity value
+ * Unused (inlined)
+ */
+// getTextOpacity () { return this.textOpacity }
+
+/**
+ * Returns the default text color for an editor content.
+ *
+ * The color value is directly read from the `TextEditorView` computed styles.
+ * @param {TextEditorElement} editorElement
+ * @param {number} textOpacity
+ * @return {string} a CSS color
+ */
+function getDefaultColor(editorElement, textOpacity) {
+}
