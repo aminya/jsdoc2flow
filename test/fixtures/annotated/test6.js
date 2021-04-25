2cb8ae16c60ac17a80ee0a8e11a85ba121e6a259
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-04-25 17:31:26 -0500

    fix: return type was not placed correctly if the parameter had = fun()

---
+++ test/fixtures/annotated/test6.js
@@ -0,0 +1,18 @@
+/** Get the type of the given path
+ * @param {string} filepath absolute path
+ * @returns {Promise<PATH_TYPE>}
+ */
+async function getPathType(filepath: string): Promise<PATH_TYPE> {
+  let pathStat
+  try {
+    pathStat = await stat(filepath)
+  } catch (error) {
+    return PATH_TYPE.OTHER
+  }
+  if (pathStat.isDirectory()) {
+    return PATH_TYPE.DIRECTORY
+  } else if (pathStat.isFile()) {
+    return PATH_TYPE.FILE
+  }
+  return PATH_TYPE.OTHER
+}
