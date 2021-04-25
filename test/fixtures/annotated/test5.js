66fe9c83a1285565331a25b14925944a70bcde31
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-04-25 16:45:52 -0500

    feat: support comments on exported functions/variables

---
+++ test/fixtures/annotated/test5.js
@@ -0,0 +1,17 @@
+import path from "path"
+
+/** Converts given path to Posix (replacing \\ with /)
+ * @param {string} givenPath   Path to convert
+ * @returns {string}          Converted filepath
+ */
+export function posixifyPath(givenPath: string): string {
+  return path.normalize(givenPath).replace(/\\/g, "/")
+}
+
+/** Converts given path to Posix (replacing \\ with /) and removing ending slashes
+ * @param {string} givenPath   Path to convert
+ * @returns {string}          Converted filepath
+ */
+export function posixifyPathNormalized(givenPath: string): string {
+  return posixifyPath(givenPath).replace(/\/$/, "")
+}
